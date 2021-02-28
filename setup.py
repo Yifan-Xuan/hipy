ed27535ba1f35b627958d2fa407c752b615e827e
Yifan-Xuan <xuanyifan@stumail.nwu.edu.cn>
2021-02-28 21:03:11 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hipy',
+      version='1.0',
+      description='A python interface to Hipparcos data',
+      author='Yifan Xuan',
+      author_email='xuanyifan@stumail.nwu.edu.cn',
+      license='MIT',
+      packages=['hipy'],
+      zip_safe=False)
