0cea862824476c66d9575348f2e96c14fefd8dcb
Yifan-Xuan <xuanyifan@stumail.nwu.edu.cn>
2021-02-28 22:29:07 +0800

    add url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='hipy',
       version='1.0',
       description='A python interface to Hipparcos data',
+      url='https://github.com/Yifan-Xuan/hipy',
       author='Yifan Xuan',
       author_email='xuanyifan@stumail.nwu.edu.cn',
       license='MIT',
